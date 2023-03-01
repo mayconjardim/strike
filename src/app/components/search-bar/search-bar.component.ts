f917de549894abde6c318be5de30591a66fc3f0e
Maycon Jardim <mayconsmill@gmail.com>
2023-02-28 22:49:53 -0300

    Search-bar onSubmit Form

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
+  constructor(private router: Router) {}
 
+  onSubmit(form: NgForm) {
+    this.router.navigate(['search', form.value.search]);
+  }
 }
