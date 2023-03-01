ac9ddf36081f1f59ce5d27c86420f9fd769f1bd9
Maycon Jardim <mayconsmill@gmail.com>
2023-02-28 23:22:55 -0300

    Implementação game service

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -1,10 +1,53 @@
+import { environment } from './../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin, map } from 'rxjs';
+import { APIResponse, Game } from '../models/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GamesService {
   constructor(private http: HttpClient) {}
+
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
+
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+
+    return this.http.get<APIResponse<Game>>(`${environment.BASE_URL}/games`, {
+      params: params,
+    });
+  }
+
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(
+      `${environment.BASE_URL}/games/${id}`
+    );
+    const gameTrailersRequest = this.http.get(
+      `${environment.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${environment.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
+  }
 }
