40d7da327d5bc13ca74cec46b181c0569ca616dc
Maycon Jardim <mayconsmill@gmail.com>
2023-03-01 20:47:39 -0300

    Adicionando HttpErrorsInterceptor

---
+++ src/app/interceptors/http-errors.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorsInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error) => {
+        console.error(error);
+        return throwError(() => new Error(error.message));
+      })
+    );
+  }
+}
