34a973b83523d4ced4440ab3dfa112be20419e9b
Maycon Jardim <mayconsmill@gmail.com>
2023-03-01 20:50:20 -0300

    Adicionando HttpHeadersInterceptor

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': '517d63f6c1msh23311f7357c9978p1e6337jsn10282094684d',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: 'e40e743af2c94b0c916a8aa618fb4473',
+      },
+    });
+    return next.handle(req);
+  }
+}
