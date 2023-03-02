a124362341bc38060c5aa7010d0057eee1b9d3a5
Maycon Jardim <mayconsmill@gmail.com>
2023-03-01 21:47:11 -0300

    Game-tabs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,9 +16,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
+import { GameTabsComponent } from './components/game-tabs/game-tabs.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent],
+  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent, GameTabsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
