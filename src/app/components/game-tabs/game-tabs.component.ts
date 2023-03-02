3f14571410a2540ac9855ef564cbb68065c9e2f9
Maycon Jardim <mayconsmill@gmail.com>
2023-03-01 21:53:44 -0300

    Game-tabs HTML, CSS e TS

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Game } from 'src/app/models/models';
 
 @Component({
-  selector: 'app-game-tabs',
+  selector: 'game-tabs',
   templateUrl: './game-tabs.component.html',
-  styleUrls: ['./game-tabs.component.scss']
+  styleUrls: ['./game-tabs.component.scss'],
 })
 export class GameTabsComponent {
+  @Input() game!: Game;
 
+  constructor() {}
 }
